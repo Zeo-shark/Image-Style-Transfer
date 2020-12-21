75a065af5de307ef9ea6660c94aad024ff55a393
Zeo-shark <sauravberaiit@gmail.com>
2020-12-21 21:47:59 +0530

    added DEFINE_integer and IMAGENET_MEAN

--- transfer.py
+++ transfer.py
@@ -10,4 +10,22 @@ from skimage.io import imsave
 
 from models.vgg import vgg_16
 
+LEARNING_RATE= 10.0
+
+LOGDIR= './logs/'
+
+FLAGS= tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_string('content','','Content image')
+tf.app.flags.DEFINE_string('style','','Style image.')
+tf.app.flags.DEFINE_string('ckpt_file','checkpoints/vgg_16.ckpt','Checkpoint file.')
+tf.app.flags.DEFINE_string('result_file','result.jpg','Result file.')
+tf.app.flags.DEFINE_string('steps',1000,'Number of steps to run.')
+tf.app.flags.DEFINE_string('resize', -1, 'Resize shorter dim of content img to this size.')
+
+IMAGENET_MEAN = [123.68, 116.779, 103.939]
+
+
+
+
 
